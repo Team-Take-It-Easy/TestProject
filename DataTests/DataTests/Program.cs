7e4b2d54ee62060d444421ab8f31716416f0605a
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-06-05 12:21:01 +0300

    Added files

---
+++ DataTests/DataTests/Program.cs
@@ -0,0 +1,38 @@
+﻿using PostgresData.Models;
+using SQLData;
+using SQLData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataTests
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var sqlContext = new SQLDBContext();
+
+            var postgresContext = new PostgresDbContext();
+            var reservation = new Reservation("reservation");
+
+            var user = new User("user");
+
+            reservation.UserId = 1;
+
+            //user.Reservations.Add(reservation);
+
+            postgresContext.Reservations.Add(reservation);
+
+            postgresContext.SaveChanges();
+
+            sqlContext.Users.Add(user);
+
+            sqlContext.SaveChanges();
+
+        }
+    }
+}
